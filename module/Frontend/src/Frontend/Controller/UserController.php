435fd0e5e56b1b1f2fa5b98520d587c2fa67b1e5
Chien Nguyen <ngocchien01@gmail.com>
2016-05-08 00:40:21 +0700

    fix

--- module/Frontend/src/Frontend/Controller/UserController.php
+++ module/Frontend/src/Frontend/Controller/UserController.php
@@ -505,10 +505,6 @@ class UserController extends MyController {
                 try {
                     $response = $facebookClient->get('/me?' . $fbInfo['field_profile'], $accessToken);
                     $userInfoFacebook = $response->getGraphUser();
-                    echo '<pre>';
-                    print_r($userInfoFacebook);
-                    echo '</pre>';
-                    die();
                 } catch (\Exception $exc) {
                         echo $exc->getMessage();
                         die();
