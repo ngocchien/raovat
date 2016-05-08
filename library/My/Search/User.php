fed2a55ed14c82364420b8ca1492ed0a50ea2857
Chien Nguyen <ngocchien01@gmail.com>
2016-05-08 23:31:59 +0700

    fix

--- library/My/Search/User.php
+++ library/My/Search/User.php
@@ -281,10 +281,10 @@ class User extends SearchAbstract {
             $boolQuery->addMustNot($addQuery);
         }
         
-        echo '<pre>';
-        print_r($boolQuery);
-        echo '</pre>';
-        die();
+//        echo '<pre>';
+//        print_r($boolQuery);
+//        echo '</pre>';
+//        die();
         return $boolQuery;
     }
 
