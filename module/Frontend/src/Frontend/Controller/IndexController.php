8d7aae8ec5fea5a11f902a8fc3aa7df2a3c254bd
ChienNguyen <chiennguyenngoc@hhdgroup.com>
2016-05-13 14:04:56 +0700

    fix

--- module/Frontend/src/Frontend/Controller/IndexController.php
+++ module/Frontend/src/Frontend/Controller/IndexController.php
@@ -9,17 +9,17 @@ class IndexController extends MyController {
     /* @var $serviceProduct \My\Models\Product */
 
     public function __construct() {
-//        $this->externalCSS = [
-//            STATIC_URL . '/f/v1/css/??owl.carousel.css',
-//            STATIC_URL . '/f/v1/css/??owl.theme.default.css',
-//            STATIC_URL . '/f/v1/css/??layerslider.css',
-//        ];
-//        $this->externalJS = [
-//            STATIC_URL . '/f/v1/js/library/??owl.carousel.js',
-//            STATIC_URL . '/f/v1/js/library/??default.js',
-//            STATIC_URL . '/f/v1/js/library/??greensock.js',
-//            STATIC_URL . '/f/v1/js/library/??layerslider.kreaturamedia.jquery.js'
-//        ];
+        $this->externalCSS = [
+            STATIC_URL . '/f/v1/css/??owl.carousel.css',
+            STATIC_URL . '/f/v1/css/??owl.theme.default.css',
+            STATIC_URL . '/f/v1/css/??layerslider.css',
+        ];
+        $this->externalJS = [
+            STATIC_URL . '/f/v1/js/library/??owl.carousel.js',
+            STATIC_URL . '/f/v1/js/library/??default.js',
+            STATIC_URL . '/f/v1/js/library/??greensock.js',
+            STATIC_URL . '/f/v1/js/library/??layerslider.kreaturamedia.jquery.js'
+        ];
     }
 
     public function indexAction() {
